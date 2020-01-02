fd707df2cbf5f5f0b33f97ed725c613a4d11c9ea
PriyankaBhiogade <bhiogadepriyanka93@gmail.com>
2020-01-02 16:02:41 +0530

    Added: Test for throwing exception when car not park

--- test/parkingLotTest.js
+++ test/parkingLotTest.js
@@ -2,7 +2,15 @@ let assert = require('assert');
 let ParkingLot = require('../main/parkingLot')
 
 describe(`Parking Lot problem Test`, () => {
-    it(`given car when car is parked return true  `, () => {
+    it(`given car when car is parked return true `, () => {
         assert.ok(new ParkingLot().park(new Object));
+    })
+
+    it(`given car when car not parked should throwing an exception `, () => {
+        try {
+            assert.ok(new ParkingLot().park());
+        } catch (err) {
+            assert.equal(err.message, "Car not parked")
+        }
     })
 })
