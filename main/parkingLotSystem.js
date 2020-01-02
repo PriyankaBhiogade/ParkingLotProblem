36fd8cc5cc320507e633da99474aed40894addf8
PriyankaBhiogade <bhiogadepriyanka93@gmail.com>
2020-01-02 19:34:05 +0530

    Refactor code : Work in progress

--- main/parkingLotSystem.js
+++ main/parkingLotSystem.js
@@ -1,32 +1,42 @@
 let ParkingLotOwner = require('./ParkingLotOwner');
 let AirportSecurity = require('./AirportSecurity');
+
 class ParkingLotSystem {
     parkVehicle = [];
+    capacity = 1;
+
     park(vehicle) {
-        let counter = 0;
         let parkingLotOwner = new ParkingLotOwner()
         let airportSecurity = new AirportSecurity();
         if (vehicle === null || vehicle === undefined) {
             throw Error("Car not parked..")
         } else {
-            if(counter < 3){
-                this.parkVehicle = vehicle;
-                counter++;
+            if (this.isFull()) {
+                this.parkVehicle.push(vehicle);
                 return true;
-            }else{
+            } else {
                 parkingLotOwner.notification();
                 airportSecurity.notification();
                 throw Error("Parking lot is full....")
             }
         }
     }
-        unPark(vehicle) {
-            if (this.parkVehicle == vehicle) {
-                this.parkVehicle = null;
-                return true;
-            }else
-                throw Error("Car Not Found..")
-        }
+
+    unPark(vehicle) {
+        if (!this.isEmpty()) {
+            this.parkVehicle.pop();
+            return true;
+        } else
+            throw Error("Car Not Found..")
     }
+
+    isFull() {
+        return this.parkVehicle.length <= this.capacity;
+    }
+
+    isEmpty() {
+        return this.parkVehicle.length == 0;
+    }
+}
 
 module.exports = ParkingLotSystem;
