12640eee0a21c875f8336c11d9d38ae8ee8aa22b
PriyankaBhiogade <bhiogadepriyanka93@gmail.com>
2020-01-02 17:53:40 +0530

    Added: Functionality to upark car

--- main/parkingLot.js
+++ main/parkingLot.js
@@ -1,13 +1,19 @@
-
 class ParkingLot {
     park(vehicle) {
         if (vehicle === null || vehicle === undefined) {
-            throw Error("Car not parked")
-        }
-        else {
+            throw Error("Car not parked..")
+        } else {
             this.parkVehicle = vehicle;
             return true;
         }
     }
-}
+        unPark(vehicle) {
+            if (this.parkVehicle == vehicle) {
+                this.parkVehicle = null;
+                return true;
+            }else
+                throw Error("Car Not Found..")
+        }
+    }
+
 module.exports = ParkingLot;
