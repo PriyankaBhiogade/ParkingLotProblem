ff379421ff6fe44c89d20c192a8788ee8f333025
PriyankaBhiogade <bhiogadepriyanka93@gmail.com>
2020-01-02 17:53:00 +0530

    Added: Test To unpark car

--- test/parkingLotTest.js
+++ test/parkingLotTest.js
@@ -1,16 +1,39 @@
 let assert = require('assert');
 let ParkingLot = require('../main/parkingLot')
 
+
 describe(`Parking Lot problem Test`, () => {
-    it(`given car when car is parked return true `, () => {
-        assert.ok(new ParkingLot().park(new Object));
+    let parkingLot;
+    beforeEach(() => {
+        parkingLot = new ParkingLot();
+    })
+
+    it(`given car when car is parked should return true `, () => {
+        assert.ok(parkingLot.park(new Object));
     })
 
     it(`given car when car not parked should throwing an exception `, () => {
         try {
-            assert.ok(new ParkingLot().park());
+            assert.ok(parkingLot.park());
         } catch (err) {
-            assert.equal(err.message, "Car not parked")
+            assert.equal(err.message, "Car not parked..")
+        }
+    })
+
+    it(`given car when parked car is un parked should return true`, () => {
+        let car = new Object();
+        parkingLot.park(car);
+        assert.ok(parkingLot.unPark(car));
+    })
+
+    it(`given car when that car not found should throwing an exception`, () => {
+        try {
+            let car1 = new Object();
+            let car2 = new Object();
+            parkingLot.park(car1);
+            assert.ok(parkingLot.unPark(car2));
+        }catch(err){
+            assert.equal(err.message,"Car Not Found..")
         }
     })
 })
